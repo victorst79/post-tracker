86cea4fd5c97fdade9ca242a1fe385b31a2791d5
Victor Santaella <victorst79@gmail.com>
2024-02-14 21:20:11 +0100

    inial commit

---
+++ components/Navbar/Navbar.tsx
@@ -0,0 +1,14 @@
+export default function Navbar() {
+    return (
+        <nav>
+            <ul>
+                <li>
+                    <a href="/">Home</a>
+                </li>
+                <li>
+                    <a href="/tracks">My tracks</a>
+                </li>
+            </ul>
+        </nav>
+    )
+}
