c8fad1ea66d30866b48c93597eee775904938fa3
Victor <victorst79@gmail.com>
2024-08-14 17:52:58 +0200

    chore: Update dependencies and add Notion client library

---
+++ app/api/feedback/route.ts
@@ -0,0 +1,43 @@
+import { NextResponse, type NextRequest } from 'next/server';
+import { Client } from '@notionhq/client';
+
+const notion = new Client({
+    auth: process.env.NOTION_TOKEN,
+});
+
+export async function PATCH(req: NextRequest) {
+    try {
+        const { email, feedback, trackID } = await req.json();
+        console.log(email, feedback, trackID);
+        const dateSubmitted = new Date().toISOString();
+
+        if (!email || !feedback || !trackID) {
+            return NextResponse.json({ message: 'Email, feedback, and trackID are required' }, { status: 400 });
+        }
+
+        const feedbackTableId = process.env.FEEDBACK_TABLE_ID;
+
+        if (!feedbackTableId) {
+            throw new Error('Feedback table ID is not defined.');
+        }
+
+        const response = await notion.pages.create({
+            parent: {
+                database_id: feedbackTableId,
+            },
+            properties: {
+                "email": { "title": [{ "text": { "content": email } }] },
+                "feedback": { "rich_text": [{ "text": { "content": feedback } }] },
+                "date_submitted": { "rich_text": [{ "text": { "content": dateSubmitted } }] },
+                "track_id": { "rich_text": [{ "text": { "content": trackID } }] }
+            }
+        });
+
+        console.log(response);
+        return NextResponse.json({ message: 'Feedback updated successfully!', result: response }, { status: 200 });
+
+    } catch (error) {
+        console.error('Error updating feedback:', error);
+        return NextResponse.json({ message: 'Internal Server Error' }, { status: 500 });
+    }
+}
